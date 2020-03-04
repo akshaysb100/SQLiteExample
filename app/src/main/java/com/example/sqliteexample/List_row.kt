6f00f1c8db44391e42b52d836acf9a374d8c4c7e
Akshay Sanjay Bavalekar <akshaysb100@Akshays-MacBook-Pro.local>
2020-03-04 11:04:12 +0530

    SQLite Exaple

---
+++ app/src/main/java/com/example/sqliteexample/List_row.kt
@@ -0,0 +1,12 @@
+package com.example.sqliteexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class List_row : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_row)
+    }
+}
